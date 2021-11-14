6f4e2f30c726f7fbdc64516037e83544c31d902a
panutat-p <pp.panutat@gmail.com>
2021-11-14 22:44:24 +0700

    init npm, install dependencies

---
+++ datastores/config.js
@@ -0,0 +1,6 @@
+export const MONGO_PREFIX = "mongodb+srv";
+export const MONGO_CLUSTER = "cluster0.x6xvn.mongodb.net";
+export const MONGODB_USERNAME = "mongoAtlas";
+export const MONGODB_PASSWORD = "3FjlBdKq8L4rtGSN";
+export const MONGODB_TEST = "test";
+export const MONGODB_JOHN_FARM = "john-farm";
