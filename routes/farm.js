1bb3e59356b88646677235cfbb89c871d5889574
panutat-p <pp.panutat@gmail.com>
2021-11-15 20:31:53 +0700

    GET /farms/:id /products/:id

--- routes/farm.js
+++ routes/farm.js
@@ -3,7 +3,14 @@ import { Farm } from "../models/farm.js";
 export const initFarmRoutes = (express) => {
   express.get("/farms", async (req, res) => {
     console.log("/farms");
-    const farms = await Farm.find({});
+    const farms = await Farm.find({}, { __v: 0 });
     res.send(farms);
+  });
+
+  express.get("/farms/:id", async (req, res) => {
+    console.log("/farms/:id");
+    const id = req.params.id;
+    const farm = await Farm.findById(id, { __v: 0 }).populate("products");
+    res.send(farm);
   });
 };
--- routes/product.js
+++ routes/product.js
@@ -3,7 +3,14 @@ import { Product } from "../models/product.js";
 export const initProductRoutes = (express) => {
   express.get("/products", async (req, res) => {
     console.log("/product");
-    const products = await Product.find({});
+    const products = await Product.find({}, { __v: 0 });
     res.send(products);
+  });
+
+  express.get("/products/:id", async (req, res) => {
+    console.log("products/:id");
+    const id = req.params.id;
+    const farm = await Product.findById(id, { __v: 0 }).populate("farm");
+    res.send(farm);
   });
 };
